4f11060db3f43edd4029b987db5ecc7cee117f53
Michail Selivanov <selmichail@gmail.com>
2021-11-14 11:33:38 +0300

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by Михаил Селиванов on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
